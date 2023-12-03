8d1eafc675b1abd29b1ae7485a9da3a835a6fcff
Ashley Phan <avvp2612@gmail.com>
2023-12-03 13:31:22 -0500

    link js file

--- src/question.js
+++ src/question.js
@@ -1,1 +1,16 @@
-console.log("print")
+//array of objects
+
+const quiz = [ 
+{
+    question: "THE MINIMUM DRINKING AGE IN THIS STATE IS ____ YEARS.",
+    options: ["9", "20", "21", "18" ],
+    answer: 2
+},
+{
+    question: "PEOPLE UNDER 16 YEARS OF AGE WHO USE A FALSE IDENTIFICATION CARD TO BUY ALCOHOL WILL.",
+    options: ["Receive a driving suspension that starts on their 16th birthday", "Not be able to take the driver’s exam until their 21st birthday", "Receive a driving suspension that starts on their 21st birthday", "Be sent to an alcohol safety education class" ],
+    answer: 0
+},
+
+
+]
