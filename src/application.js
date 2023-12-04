f693d5f6a65aa69f3e9174b68f79c4d8feba0cfb
Ashley Phan <avvp2612@gmail.com>
2023-12-04 18:52:36 -0500

    Updating minors

--- src/application.js
+++ src/application.js
@@ -14,12 +14,17 @@ function setAvaibleQuestions(){
         avaibleQuestions.push(quiz[i])
     }
 
-
-
 }
 
 function getNewQuestion(){
     questionNumber.innerHTMK = "Question " + (questionCounter + 1) + "of" + quiz.length;
+
+    // set question text
+    // get random question
+
+    const questionIndex = avaibleQuestions[Math.floor(Math.random() * avaibleQuestions.length())]; 
+    currentQuestion = questionIndex;
+    questionText.innerHTML = currentQuestion.q;
 }
 
 window.onload = function(){
